98667e04302a8b517a075c4e0dbd5211f70b9103
Aditya Chavan <adityachavan095@gmail.com>
2021-12-31 23:54:03 +0530

    Done 3

---
+++ Goldman Sachs/3.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        
+        if(k == 0)
+            return 0;
+        int count =0;
+        long long int prod =1;
+        int j =0;
+        for(int i=0;i<n;i++){
+            prod *= a[i];
+            
+            while(j <= i && prod >= k){
+                prod /= a[j];
+                j++;
+            }
+            
+            count += i-j+1;
+            
+        }
+        
+        return count;
+        
+        
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
