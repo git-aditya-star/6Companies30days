e375a06896d928279e6dab53197bac64215d392f
Aditya Chavan <adityachavan095@gmail.com>
2022-01-16 12:59:08 +0530

    Done 6

---
+++ Microsoft/6.cpp
@@ -0,0 +1,85 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    void solve(int i, string res,unordered_map<int,string>hmap, int a[], int N, vector<string>&ans)
+    {
+        if(i>=N){
+            ans.push_back(res);
+            return;
+        }
+        int key=a[i];  
+        for(char ch: hmap[key]){ 
+            res+=ch; 
+            solve(i+1,res,hmap,a,N,ans); 
+      
+            res.pop_back();
+        }
+        
+        
+        
+    }
+    //Function to find list of all words possible by pressing given numbers.
+    vector<string> possibleWords(int a[], int N)
+    {
+        //Your code here
+        unordered_map<int,string>hmap;
+        hmap[1]="";
+        hmap[2]="abc";
+        hmap[3]="def";
+        hmap[4]="ghi";
+        hmap[5]="jkl";
+        hmap[6]="mno";
+        hmap[7]="pqrs";
+        hmap[8]="tuv";
+        hmap[9]="wxyz";
+        hmap[10]="*";
+        hmap[11]="0";
+        hmap[12]="#";
+        string res="";
+        vector<string> ans;
+        solve(0,res,hmap,a,N,ans);
+        return ans;
+    }
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+  
+	int T;
+	
+	cin >> T; //testcases
+	
+	while(T--){ //while testcases exist
+	   int N;
+	    
+	   cin >> N; //input size of array
+	   
+	   int a[N]; //declare the array
+	   
+	   for(int i =0;i<N;i++){
+	       cin >> a[i]; //input the elements of array that are keys to be pressed
+	   }
+	   
+	   Solution obj;
+	   
+	  vector <string> res = obj.possibleWords(a,N);
+	  for (string i : res) cout << i << " ";
+	   cout << endl;
+	}
+	
+	return 0;
+}  // } Driver Code Ends
